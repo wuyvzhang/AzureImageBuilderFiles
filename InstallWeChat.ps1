192de58928e1c4ff1ca0b71454292deec6b9cd80
Gavin Wu <39542567+wuyvzhang@users.noreply.github.com>
2021-10-28 15:40:28 +0800

    Create InstallWeChat.ps1

---
+++ InstallWeChat.ps1
@@ -0,0 +1,15 @@
+ $appName = 'teams'
+ $drive = 'C:\'
+ New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
+ $LocalPath = $drive + '\' + $appName 
+ set-Location $LocalPath
+
+
+
+ write-host 'AIB Customization: Install Wechat'
+ $WeComURL = 'https://aibstore.blob.core.windows.net/software/WeCom_3.1.18.6007.exe'
+ $WeComEXE = 'WeCom.exe'
+ $outputPath = $LocalPath + '\' + $WeComEXE
+ Invoke-WebRequest -Uri $WeComURL -OutFile $outputPath
+ Start-Process -FilePath WeCom.exe -Args "/I $outputPath /S" -Wait
+ write-host 'AIB Customization: Finished Install WeComEXE' 
